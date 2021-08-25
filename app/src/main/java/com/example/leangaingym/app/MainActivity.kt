1bf5d408f7e73ff2734ae773843f1b4f939988c0
Aleksey Petrov <Alekseyandreevichpetrov304@gmail.com>
2021-08-25 17:24:35 +0300

    row layout Implementation

--- app/src/main/java/com/example/leangaingym/app/MainActivity.kt
+++ app/src/main/java/com/example/leangaingym/app/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.leangaingym
+package com.example.leangaingym.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.leangaingym.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
