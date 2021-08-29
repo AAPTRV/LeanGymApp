d70b29082c9ee2d3bd29c9cda662bd4ed1bc35b2
Aleksey Petrov <Alekseyandreevichpetrov304@gmail.com>
2021-08-29 18:52:48 +0300

    Hide keyboard after dialog and IME action done button implemented

--- app/src/main/java/com/example/leangaingym/ext/dialogExt/dialogExt.kt
+++ app/src/main/java/com/example/leangaingym/ext/dialogExt/dialogExt.kt
@@ -2,14 +2,18 @@ package com.example.leangaingym.ext.dialogExt
 
 import android.app.Activity
 import android.app.Dialog
+import android.content.Context.INPUT_METHOD_SERVICE
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.Window
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.FrameLayout
+import android.widget.TextView
 import com.example.leangaingym.R
 import com.example.leangaingym.dto.TemplateExerciseUnitDto
 
@@ -46,33 +50,76 @@ private fun setContentView(dialog: Dialog, contentView: View) {
 }
 
 fun Activity.showDialogForExerciseResult(
-//    leftButtonTextId: Int,
     leftOnClickEvent: (TemplateExerciseUnitDto) -> Unit,
-//    rightButtonTextId: Int,
-//    rightClickListener: View.OnClickListener?
-) : Dialog{
+): Dialog {
     val (dialog, contentView) = initExerciseDialogTwoButtonsContent()
 
     val mEtExerciseName: EditText = contentView.findViewById(R.id.dialogEtExerciseName)
-    val mEtNumberOfApproaches: EditText = contentView.findViewById(R.id.dialogEtApproaches)
-    val mEtNumberOfRepetitions: EditText = contentView.findViewById(R.id.dialogEtRepetitions)
+    mEtExerciseName.imeOptions = EditorInfo.IME_ACTION_DONE
+    mEtExerciseName.setOnEditorActionListener { _, actionId, _ ->
+        if (actionId == EditorInfo.IME_ACTION_DONE) {
+            // Hide Keyboard
+            val inputMethodManager = this.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(contentView.windowToken, 0)
+        }
+        false
+    }
+
+    val mTvApproachesValue: TextView = contentView.findViewById(R.id.dialogApproachesValue)
+    val mBtnApproachesIncrease: Button =
+        contentView.findViewById(R.id.dialogIncreaseApproachesButton)
+    val mBtnApproachesDecrease: Button =
+        contentView.findViewById(R.id.dialogDecreaseApproachesButton)
+
+    val mTvRepetitionsValue: TextView = contentView.findViewById(R.id.dialogRepetitionsValue)
+    val mBtnRepetitionsIncrease: Button =
+        contentView.findViewById(R.id.dialogIncreaseRepetitionsButton)
+    val mBtnRepetitionsDecrease: Button =
+        contentView.findViewById(R.id.dialogDecreaseRepetitionsButton)
+
+    mBtnApproachesIncrease.setOnClickListener {
+        var value = mTvApproachesValue.text.toString().toInt()
+        value++
+        mTvApproachesValue.text = value.toString()
+    }
+    mBtnApproachesDecrease.setOnClickListener {
+        var value = mTvApproachesValue.text.toString().toInt()
+        if (value > 1) {
+            value--
+        }
+        mTvApproachesValue.text = value.toString()
+    }
+
+    mBtnRepetitionsIncrease.setOnClickListener {
+        var value = mTvRepetitionsValue.text.toString().toInt()
+        value++
+        mTvRepetitionsValue.text = value.toString()
+    }
+    mBtnRepetitionsDecrease.setOnClickListener {
+        var value = mTvRepetitionsValue.text.toString().toInt()
+        if (value > 1) {
+            value--
+        }
+        mTvRepetitionsValue.text = value.toString()
+    }
 
     val btnLeft: Button = contentView.findViewById(R.id.dialogLeftButton)
     btnLeft.setOnClickListener {
         leftOnClickEvent.invoke(
             TemplateExerciseUnitDto(
                 mExerciseName = mEtExerciseName.text.toString(),
-                mNumberOfApproaches = mEtNumberOfApproaches.text.toString().toInt(),
-                mNumberOfRepetitions = mEtNumberOfRepetitions.text.toString().toInt()
+                mNumberOfApproaches = mTvApproachesValue.text.toString().toInt(),
+                mNumberOfRepetitions = mTvRepetitionsValue.text.toString().toInt()
             )
         )
-        dialog.dismiss() }
+        dialog.dismiss()
+    }
 
     val btnRight: Button = contentView.findViewById(R.id.dialogRightButton)
     btnRight.setOnClickListener { dialog.dismiss() }
 
     setContentView(dialog, contentView)
-    if(!this.isFinishing){
+    if (!this.isFinishing) {
         dialog.show()
     }
     return dialog
